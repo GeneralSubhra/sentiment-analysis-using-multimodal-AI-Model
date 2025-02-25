86744af53d0fdff6bbc2fa81180a23f482fcd767
Subhranil Paul <107763762+GeneralSubhra@users.noreply.github.com>
2025-02-25 23:09:04 +0530

    text and video encoder in models

---
+++ training/models.py
@@ -0,0 +1,40 @@
+import torch.nn as nn 
+from transformers import BertModel
+from torchvision import models as vision_models
+
+
+class TextEncoder(nn.modele):
+    def __init__(self):
+        super().__init__()
+        self.bert= BertModel.from_pretrained('bert-base-uncased')
+        for param in self.bert.parameters():
+            param.requires_grad = False
+            
+        self.projection = nn.Linear(768,128)
+        
+    def forward (self,input_ids,attention_mask):
+        #Extract bert embeddings
+        outputs= self.bert(input_ids=input_ids,attention_mask=attention_mask)
+        #use [CLS] token representation
+        pooled_output = outputs.pooled_output
+        return self.projection(pooled_output)
+        
+class VideoEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.backbone = vision_models.video.r3d_18(pretrained=True)
+        
+        for param in self.backbone.parameters():
+            param.requires_grad = False
+        
+        num_fts=self.backbone.fc.in_features
+        self.backbone.fc=nn.Sequential(
+            nn.Linear(num_fts,128),
+            nn.ReLU(),
+            nn.Dropout(0.2)
+        )
+    
+    def forward(self,x):
+        x = x.transpose(1,2)
+            
+            
